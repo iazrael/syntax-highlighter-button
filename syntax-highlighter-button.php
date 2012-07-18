b88cc92de2bdae18d63b8a96ca1f684f2a87d7fe
azrael <azrael@imatlas.com>
2012-07-18 09:54:59 +0800

    update

--- syntax-highlighter-button.php
+++ syntax-highlighter-button.php
@@ -1,11 +1,28 @@
 <?php
 /*
 Plugin Name: Syntax Highlighter Button
-Plugin URI: http://imatlas.com/
+Plugin URI: http://imatlas.com/posts/quick-code-button-wordpress/
 Description: 给Wordpress的编辑器增加插入Syntax Highlighter代码的按钮
 Version: 1.0
 Author: imatlas
 Author URI: http://imatlas.com/
+*/
+
+/*  Copyright 2012  imatlas  (email : atlas@imatlas)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 function insert_code_button(){
